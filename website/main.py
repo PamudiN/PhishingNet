efe1072701317482931ccf42c9c8bd3373d4e52d
Pamudi Navodya <129357978+PamudiN@users.noreply.github.com>
2023-05-29 11:45:05 +0530

    final product-phishing net

---
+++ website/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template
+import numpy as np
+from sklearn import metrics
+import warnings
+import pickle
+from templates.features import FeatureExtraction
+
+warnings.filterwarnings('ignore')
+
+app = Flask(__name__)
+
+# Load the model from the file
+with open('F:/Projects/Phising Platform/website/pickle/model.pkl', 'rb') as file:
+    gbc = pickle.load(file)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1, 30)
+
+        y_pred = gbc.predict(x)[0]
+        y_pro_phishing = gbc.predict_proba(x)[0, 1]
+        y_pro_non_phishing = gbc.predict_proba(x)[0, 0]
+
+        # Format the prediction message
+        if y_pred == 1:
+            pred = "It is {0:.2f}% safe to go".format(y_pro_non_phishing * 100)
+        else:
+            pred = "It is {0:.2f}% unsafe".format(y_pro_phishing * 100)
+
+        return render_template('index.html', xx=round(y_pro_non_phishing, 2), url=url, prediction=pred)
+
+    return render_template("index.html", xx=-1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
